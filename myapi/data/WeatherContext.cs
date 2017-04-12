c92e331972d8e5053887da062b5689e47a520470
Andy Barker <abarker@swankmp.com>
2017-04-12 16:07:04 -0500

    context changes

---
+++ myapi/data/WeatherContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace myapi.data
+{
+    public class WeatherContext : DbContext
+    {
+        public WeatherContext(DbContextOptions<WeatherContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WeatherEvent> WeatherEvents { get; set; }
+        public DbSet<Reaction> Reactions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
